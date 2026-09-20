2950c42566de17ef07178bee47d4a380205ba1fc
aosamaesmail <aosamaesmail@gmail.com>
2026-09-20 14:55:32 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'telecomax-km-v34';
+const CACHE = 'telecomax-km-v35';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
